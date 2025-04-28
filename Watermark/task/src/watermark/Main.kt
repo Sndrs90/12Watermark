bf939e9f781a156cc77f2c4c05375fb8606eb766
Sanders <sndrs90@gmail.com>
2025-04-28 14:34:38 +0300

    Stage 5 completed

--- Watermark/task/src/watermark/Main.kt
+++ Watermark/task/src/watermark/Main.kt
@@ -49,19 +49,47 @@ fun main() {
     val weight = getWeight()
     if (!isWeightValid(weight)) return
 
-    val outputPath = getOutputPath()
-    if (!isOutputExtensionValid(outputPath)) return
-    val extension = outputPath.substringAfterLast('.')
-    val output = BufferedImage(image.width, image.height, BufferedImage.TYPE_INT_RGB)
+    val positionMethod = getPositionMethod()
+    if (!isPositionMethodValid(positionMethod)) return
+    when (positionMethod) {
+        "single" -> {
+            val diffX = image.width - watermark.width
+            val diffY = image.height - watermark.height
+            println("Input the watermark position ([x 0-$diffX] [y 0-$diffY]):")
+            val wmPositionInput = readln()
+            if (!isWatermarkPositionInputCorrect(wmPositionInput, diffX, diffY)) return
+            val (posX, posY) = wmPositionInput.split(" ").map { it.toInt() }
 
-    when (input) {
-        "yes" -> {
-            if (watermark.transparency == Transparency.TRANSLUCENT) applyTransparent(image, watermark, output, weight)
-            else applyTransparentColor(inputColor, image, watermark, output, weight)
+            val outputPath = getOutputPath()
+            if (!isOutputExtensionValid(outputPath)) return
+            val extension = outputPath.substringAfterLast('.')
+            val output = BufferedImage(image.width, image.height, BufferedImage.TYPE_INT_RGB)
+
+            when (input) {
+                "yes" -> {
+                    if (watermark.transparency == Transparency.TRANSLUCENT) applySingleTransparent(image, watermark, output, weight, posX, posY)
+                    else applySingleTransparentColor(inputColor, image, watermark, output, weight, posX, posY)
+                }
+                else -> applySingleWatermark(image, watermark, output, weight, posX, posY)
+            }
+            saveOutput(output, extension, outputPath)
         }
-        else -> applyWatermark(image, watermark, output, weight)
+        "grid" -> {
+            val outputPath = getOutputPath()
+            if (!isOutputExtensionValid(outputPath)) return
+            val extension = outputPath.substringAfterLast('.')
+            val output = BufferedImage(image.width, image.height, BufferedImage.TYPE_INT_RGB)
+
+            when (input) {
+                "yes" -> {
+                    if (watermark.transparency == Transparency.TRANSLUCENT) applyGridTransparent(image, watermark, output, weight)
+                    else applyGridTransparentColor(inputColor, image, watermark, output, weight)
+                }
+                else -> applyGridWatermark(image, watermark, output, weight)
+            }
+            saveOutput(output, extension, outputPath)
+        }
     }
-    saveOutput(output, extension, outputPath)
 }
 
 private fun getImagePath(): String {
@@ -104,19 +132,19 @@ private fun isWatermarkValid(watermark: BufferedImage, image: BufferedImage): Bo
         return false
     }
 
-    if (!areEqualDimensions(image, watermark)) {
-        println("The image and watermark dimensions are different.")
+    if (!areEqualOrLessDimensions(image, watermark)) {
+        println("The watermark's dimensions are larger.")
         return false
     }
     return true
 }
 
-private fun areEqualDimensions(image: BufferedImage, watermark: BufferedImage): Boolean {
+private fun areEqualOrLessDimensions(image: BufferedImage, watermark: BufferedImage): Boolean {
     val imHeight = image.height
     val imWidth = image.width
     val wmHeight = watermark.height
     val wmWidth = watermark.width
-    return imHeight == wmHeight && imWidth == wmWidth
+    return wmHeight <= imHeight && wmWidth <= imWidth
 }
 
 private fun getWeight(): Int {
@@ -137,6 +165,36 @@ private fun isWeightValid(weight: Int): Boolean {
     return true
 }
 
+private fun getPositionMethod(): String {
+    println("Choose the position method (single, grid):")
+    return readln()
+}
+
+private fun isPositionMethodValid(positionMethod: String): Boolean {
+    when (positionMethod) {
+        "single", "grid" -> return true
+        else -> {
+            println("The position method input is invalid.")
+            return false
+        }
+    }
+}
+
+private fun isWatermarkPositionInputCorrect(wmPositionInput: String, diffX: Int, diffY: Int): Boolean {
+    val regex = "^-?\\d+\\s-?\\d+\$".toRegex()
+    if (regex.matches(wmPositionInput)) {
+        val (x, y) = wmPositionInput.split(" ").map { it.toInt() }
+        if (x in 0..diffX && y in 0..diffY) return true
+        else {
+            println("The position input is out of range.")
+            return false
+        }
+    } else {
+        println("The position input is invalid.")
+        return false
+    }
+}
+
 private fun getOutputPath(): String {
     println("Input the output image filename (jpg or png extension):")
     return readln()
@@ -150,17 +208,52 @@ private fun isOutputExtensionValid(outputPath: String): Boolean {
     return true
 }
 
-private fun applyWatermark(
+private fun applySingleWatermark(
     image: BufferedImage,
     watermark: BufferedImage,
     output: BufferedImage,
-    weight: Int
+    weight: Int,
+    posX: Int,
+    posY: Int
 ) {
     for (y in 0 until image.height) {
         for (x in 0 until image.width) {
             try {
                 val i = Color(image.getRGB(x, y))
-                val w = Color(watermark.getRGB(x, y))
+                if ((x - posX) in 0 until watermark.width && (y - posY) in 0 until watermark.height) {
+                    val w = Color(watermark.getRGB(x, y))
+                    val color = Color(
+                        (weight * w.red + (100 - weight) * i.red) / 100,
+                        (weight * w.green + (100 - weight) * i.green) / 100,
+                        (weight * w.blue + (100 - weight) * i.blue) / 100
+                    )
+                    output.setRGB(x, y, color.rgb)
+                } else {
+                    val color = Color(i.red, i.green, i.blue)
+                    output.setRGB(x, y, color.rgb)
+                }
+            } catch (e: Exception) {
+                println("An error occurred at pixel ($x, $y)")
+            }
+        }
+    }
+}
+
+private fun applyGridWatermark(
+    image: BufferedImage,
+    watermark: BufferedImage,
+    output: BufferedImage,
+    weight: Int
+) {
+    val watermarkWidth = watermark.width
+    val watermarkHeight = watermark.height
+    for (y in 0 until image.height) {
+        for (x in 0 until image.width) {
+            try {
+                val i = Color(image.getRGB(x, y))
+                val gridX = x % watermarkWidth
+                val gridY = y % watermarkHeight
+                val w = Color(watermark.getRGB(gridX, gridY))
                 val color = Color(
                     (weight * w.red + (100 - weight) * i.red) / 100,
                     (weight * w.green + (100 - weight) * i.green) / 100,
@@ -174,24 +267,30 @@ private fun applyWatermark(
     }
 }
 
-private fun applyTransparent(
+private fun applySingleTransparent(
     image: BufferedImage,
     watermark: BufferedImage,
     output: BufferedImage,
-    weight: Int
+    weight: Int,
+    posX: Int,
+    posY: Int
 ) {
     for (y in 0 until image.height) {
         for (x in 0 until image.width) {
             try {
                 val i = Color(image.getRGB(x, y))
-                val w = Color(watermark.getRGB(x, y), true)
-                val alpha = w.alpha
+                val alpha = if ((x - posX) in 0 until watermark.width && (y - posY) in 0 until watermark.height) {
+                    Color(watermark.getRGB(x - posX, y - posY), true).alpha
+                } else {
+                    0
+                }
                 when (alpha) {
                     0 -> {
                         val color = Color(i.red, i.green, i.blue)
                         output.setRGB(x, y, color.rgb)
                     }
                     else -> {
+                        val w = Color(watermark.getRGB(x - posX, y - posY), true)
                         val color = Color(
                             (weight * w.red + (100 - weight) * i.red) / 100,
                             (weight * w.green + (100 - weight) * i.green) / 100,
@@ -200,6 +299,52 @@ private fun applyTransparent(
                         output.setRGB(x, y, color.rgb)
                     }
                 }
+            } catch (e: Exception) {
+                println("An error occurred at pixel ($x, $y)")
+            }
+        }
+    }
+}
+
+private fun applyGridTransparent(
+    image: BufferedImage,
+    watermark: BufferedImage,
+    output: BufferedImage,
+    weight: Int
+) {
+    // Get the dimensions of the watermark for interval calculations
+    val watermarkWidth = watermark.width
+    val watermarkHeight = watermark.height
+
+    // Loop through the image height and width
+    for (y in 0 until image.height) {
+        for (x in 0 until image.width) {
+            try {
+                val i = Color(image.getRGB(x, y)) // Get the color of the original image pixel
+
+                // Determine the position of the watermark based on grid positioning
+                val gridX = x % watermarkWidth
+                val gridY = y % watermarkHeight
+
+                // Get the corresponding watermark pixel color, take into account the alpha channel
+                val w = Color(watermark.getRGB(gridX, gridY), true)
+
+                // Calculate the final color, taking transparency into account
+                val alpha = w.alpha
+                val finalColor: Color
+                if (alpha == 0) {
+                    // If the watermark pixel is fully transparent, retain the original pixel color
+                    finalColor = i
+                } else {
+                    // Mix the colors based on the weight provided
+                    finalColor = Color(
+                        (weight * w.red + (100 - weight) * i.red) / 100,
+                        (weight * w.green + (100 - weight) * i.green) / 100,
+                        (weight * w.blue + (100 - weight) * i.blue) / 100
+                    )
+                }
+                // Set the calculated color to the output image
+                output.setRGB(x, y, finalColor.rgb)
             } catch (e: Exception) {
                 println("An error occurred at pixel ($x, $y)")
             }
@@ -215,12 +360,15 @@ private fun isTransparencyColorValid(inputColor: String): Boolean {
     }
     return true
 }
-private fun applyTransparentColor(
+
+private fun applySingleTransparentColor(
     inputColor: String,
     image: BufferedImage,
     watermark: BufferedImage,
     output: BufferedImage,
-    weight: Int
+    weight: Int,
+    posX: Int,
+    posY: Int
 ) {
     val (red, green, blue) = inputColor.split(" ").map { it.toInt() }
     val transparentColor = Color(red, green, blue)
@@ -229,21 +377,63 @@ private fun applyTransparentColor(
         for (x in 0 until image.width) {
             try {
                 val i = Color(image.getRGB(x, y))
-                val w = Color(watermark.getRGB(x, y))
-                when (w) {
-                    transparentColor -> {
-                        val color = Color(i.red, i.green, i.blue)
-                        output.setRGB(x, y, color.rgb)
+                if ((x - posX) in 0 until watermark.width && (y - posY) in 0 until watermark.height) {
+                    val w = Color(watermark.getRGB(x - posX, y - posY))
+                    when (w) {
+                        transparentColor -> {
+                            val color = Color(i.red, i.green, i.blue)
+                            output.setRGB(x, y, color.rgb)
+                        }
+                        else -> {
+                            val color = Color(
+                                (weight * w.red + (100 - weight) * i.red) / 100,
+                                (weight * w.green + (100 - weight) * i.green) / 100,
+                                (weight * w.blue + (100 - weight) * i.blue) / 100
+                            )
+                            output.setRGB(x, y, color.rgb)
+                        }
                     }
-                    else -> {
-                        val color = Color(
+                } else {
+                    val color = Color(i.red, i.green, i.blue)
+                    output.setRGB(x, y, color.rgb)
+                }
+            } catch (e: Exception) {
+                println("An error occurred at pixel ($x, $y)")
+            }
+        }
+    }
+}
+
+private fun applyGridTransparentColor(
+    inputColor: String,
+    image: BufferedImage,
+    watermark: BufferedImage,
+    output: BufferedImage,
+    weight: Int
+) {
+    val watermarkWidth = watermark.width
+    val watermarkHeight = watermark.height
+    val (red, green, blue) = inputColor.split(" ").map { it.toInt() }
+    val transparentColor = Color(red, green, blue)
+
+    for (y in 0 until image.height) {
+        for (x in 0 until image.width) {
+            try {
+                val i = Color(image.getRGB(x, y))
+
+                val gridX = x % watermarkWidth
+                val gridY = y % watermarkHeight
+                val w = Color(watermark.getRGB(gridX, gridY))
+                val finalColor: Color = when (w) {
+                    transparentColor -> Color(i.red, i.green, i.blue)
+                    else ->
+                        Color(
                             (weight * w.red + (100 - weight) * i.red) / 100,
                             (weight * w.green + (100 - weight) * i.green) / 100,
                             (weight * w.blue + (100 - weight) * i.blue) / 100
                         )
-                        output.setRGB(x, y, color.rgb)
-                    }
                 }
+                output.setRGB(x, y, finalColor.rgb)
             } catch (e: Exception) {
                 println("An error occurred at pixel ($x, $y)")
             }
